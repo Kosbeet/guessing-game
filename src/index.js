74fffc2e62375dac7444b00a6323d7dcbd9aeddc
Konstantin <beeters88@gmail.com>
2025-08-31 20:49:39 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 class GuessingGame {
   constructor() {
-    throw new Error('Not implemented');
+    this.min = null;
+    this.max = null;
+    this.current = null;
   }
-  /*
-  setRange(min, max) { }
 
-  guess() { }
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-  lower() { }
+  guess() {
+    this.current = Math.ceil(this.min + (this.max - this.min) / 2);
+    return this.current;
+  }
 
-  greater() { }
-*/
+  lower() {
+    this.max = this.current;
+  }
+
+  greater() {
+    this.min = this.current;
+  }
 }
 
 module.exports = GuessingGame;
